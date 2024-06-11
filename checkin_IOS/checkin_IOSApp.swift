bce3a13c7aec9917c4768152e6f43d5eeb45f97f
ICHOU Mohammed <devichoumohammed@gmail.com>
2024-06-11 11:20:37 +0200

    Initial Commit

---
+++ checkin_IOS/checkin_IOSApp.swift
@@ -0,0 +1,32 @@
+//
+//  checkin_IOSApp.swift
+//  checkin_IOS
+//
+//  Created by mohammed ichou on 11/06/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct checkin_IOSApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
