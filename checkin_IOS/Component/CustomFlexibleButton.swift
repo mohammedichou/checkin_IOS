ede75840216a0d65ecf45fcabb9f7c45098d8fab
ICHOU Mohammed <devichoumohammed@gmail.com>
2024-10-02 01:46:26 +0200

    loggin page view

---
+++ checkin_IOS/Component/CustomFlexibleButton.swift
@@ -0,0 +1,18 @@
+//
+//  CustomFlexibleButton.swift
+//  checkin_IOS
+//
+//  Created by mohammed ichou on 02/10/2024.
+//
+
+import SwiftUI
+
+struct CustomFlexibleButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomFlexibleButton()
+}
---
+++ checkin_IOS/Component/CustomTitledTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTitledTextField.swift
+//  checkin_IOS
+//
+//  Created by mohammed ichou on 02/10/2024.
+//
+
+import SwiftUI
+
+struct CustomTitledTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTitledTextField()
+}
