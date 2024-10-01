a24a131d32cca682fa8404c4e08e1795b55b4616
ICHOU Mohammed <devichoumohammed@gmail.com>
2024-10-01 22:00:45 +0200

    start project

---
+++ checkin_IOS/View/Authentification/ChoiceStatutView.swift
@@ -0,0 +1,18 @@
+//
+//  ChoiceStatutView.swift
+//  checkin_IOS
+//
+//  Created by mohammed ichou on 01/10/2024.
+//
+
+import SwiftUI
+
+struct ChoiceStatutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChoiceStatutView()
+}
---
+++ checkin_IOS/View/Connexion/Choice_log_View.swift
@@ -0,0 +1,18 @@
+//
+//  Choice_log_View.swift
+//  checkin_IOS
+//
+//  Created by mohammed ichou on 12/06/2024.
+//
+
+import SwiftUI
+
+struct Choice_log_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Choice_log_View()
+}
